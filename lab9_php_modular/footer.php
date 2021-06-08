40fc659b899223389b5459fb92e48b90401d773a
antonmartinus72 <antonmartinus72@gmail.com>
2021-06-08 18:20:59 +0700

    first commit

---
+++ lab9_php_modular/footer.php
@@ -0,0 +1,7 @@
+<link href="style.css" rel="stylesheet" type="text/css">
+<footer>
+	<p>&copy; 2021, Informatika, Universitas Pelita Bangsa</p>
+</footer>
+</div>
+</body>
+</html>
---
+++ lab9_php_modular/header.php
@@ -0,0 +1,23 @@
+<?php 
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Contoh Modularisasi</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container">
+		<header>
+			<h1>Modularisasi Menggunakan Require</h1>
+		</header>
+		<nav id="nav">
+			<a href="home.php">Home</a>
+			<a href="about.php">About</a>
+			<a href="lab8_php_database/index.php
+			">index</a>
+
+
+		</nav>
